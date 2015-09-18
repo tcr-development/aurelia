5030ed2c00877f62a64b361cc8673a5a7a5c1b3e
David Kolby <david@kolby.ninja>
2015-09-18 13:30:50 -0400

    added route

--- Aurelia.Web/App_Start/WebApiConfig.cs
+++ Aurelia.Web/App_Start/WebApiConfig.cs
@@ -5,16 +5,28 @@ using System.Web.Http;
 
 namespace Aurelia.Web {
    public static class WebApiConfig {
+
+      public static string ApiControllerOnly = "ApiControllerOnly";
+      public static string ApiControllerAndId = "ApiControllerAndId";
+
       public static void Register(HttpConfiguration config) {
          // Web API configuration and services
 
          // Web API routes
          config.MapHttpAttributeRoutes();
 
+         //api/todoapi/5
          config.Routes.MapHttpRoute(
-             name: "DefaultApi",
+             name: ApiControllerAndId,
              routeTemplate: "api/{controller}/{id}",
-             defaults: new { id = RouteParameter.Optional }
+             defaults: null,
+             constraints: new { id = @"^\d+$" } // all digits
+         );
+
+         //api/todoapi
+         config.Routes.MapHttpRoute(
+             name: ApiControllerOnly,
+             routeTemplate: "api/{controller}"
          );
 
          var jsonFormatter = config.Formatters.OfType<JsonMediaTypeFormatter>().First();
